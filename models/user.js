15735a36552725189189c6f4195607a1013d8aea
Kminsang02 <rlaalstkd0212@gmail.com>
2020-05-18 20:32:20 +0900

    user.js 스키마 수정

--- models/user.js
+++ models/user.js
@@ -3,6 +3,13 @@ import { Schema, model } from 'mongoose';
 const User = new Schema({
   id: String,
   password: String,
+  birth: Number,
+  hometown: String,
+  Language: String,
+  heightcm: {
+    type: Number,
+    min: 100,
+  },
 });
 
 export default model('Users', User);
